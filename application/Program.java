3448eaecd85d47efd5b9537579678c0e1a1a8d98
MatheusMedeiros22 <mathmed22@gmail.com>
2022-05-04 18:38:39 -0300

    Aprendendo a instanciar quando se utiliza de composição de objetos

--- application/Program.java
+++ application/Program.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Departament;
+import entities.HourContract;
+import entities.Worker;
 import entities.enums.WorkerLevel;
 
 public class Program {
@@ -30,6 +32,9 @@ public class Program {
 		System.out.println("Base salary");
 		Double baseSalary = sc.nextDouble();
 		
+		Worker worker = new Worker(name, level, baseSalary, departament); // no lugar de departament, posso instanciar direto new Departament(departamentName)
+		
+		
 		System.out.println("How many contracts to this worker: ");
 		int manyContracts = sc.nextInt();
 		
@@ -37,12 +42,23 @@ public class Program {
 			System.out.println("Enter contract #" + i + " data:");
 			System.out.println("Date (DD/MM/YYYY)");
 			Date date = sdf.parse(sc.next());
-			System.out.println("Value per hour: ");
+			System.out.print("Value per hour: ");
 			double valuePerHour = sc.nextDouble();
 			System.out.println("Duration (hours): ");
 			int hours = sc.nextInt();
+			HourContract contract = new HourContract(date, valuePerHour, hours);
+			worker.addContract(contract);
 		}
 		
+		System.out.println();
+		System.out.println("Enter month and year to calculate income(MM/YYYY)");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartament().getName());
+		System.out.println("Income for " + monthAndYear + ": " + worker.income(year, month) );
 		
 		
 		
